209071cda2e4996a28fab61daee01d2382e22fc9
Caner Kuncu <warlordsw@gmail.com>
2023-07-29 12:18:27 +0300

    test8

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
 describe('template spec', () => {
-  it('passes', () => {
+  const DivContainerSelector = '[id=testCountDivContainer]'
+  const incrementButtonSelector = '[id=increment]'
+  const decrementButtonSelector = '[id=decrement]'
+  it('count is 0', () => {
     cy.visit('https://comfy-torrone-9aa30a.netlify.app/')
+    cy.get(DivContainerSelector).should('contain.text', 0)
+  })
+
+  it('multiple clicks', () => {
+    cy.visit('https://comfy-torrone-9aa30a.netlify.app/')
+    cy.get(incrementButtonSelector).click().click().click()
+    cy.get(DivContainerSelector).should('contain.text', 3)
+    cy.get(decrementButtonSelector).click().click()
+    cy.get(DivContainerSelector).should('contain.text', 1)
   })
 })
